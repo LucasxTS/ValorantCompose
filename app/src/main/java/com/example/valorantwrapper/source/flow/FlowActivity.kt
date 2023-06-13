617a473a6bca052baeec2a0c4faaeca6877ada9b
Lucas <lucastorres6431@gmail.com>
2023-06-13 20:46:13 -0300

    Initial Commit

---
+++ app/src/main/java/com/example/valorantwrapper/source/flow/FlowActivity.kt
@@ -0,0 +1,16 @@
+package com.example.valorantwrapper.source.flow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+
+class FlowActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
+
